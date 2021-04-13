b93d9bbcc5413b8e7bc9c1c062accda4e70fddd5
sarahjaynekessler <44033444+sarahjaynekessler@users.noreply.github.com>
2021-04-13 11:47:50 -0400

    new files and tables

--- code/findHalfLight.py
+++ code/findHalfLight.py
@@ -7,6 +7,7 @@ from astropy.modeling import models, fitting
 from astropy.stats import sigma_clip
 from astropy.modeling.models import Sersic1D
 import warnings
+from scipy.stats import chisquare
 warnings.filterwarnings("ignore")
 
 xnew = np.arange(0.25,2.0,.01)
@@ -125,58 +126,121 @@ def fitLogFunc(rp):
     return(rp,norm_x,norm_y,fx2,fy2,popt,pcov)
         
 def findHalfLightSersicdf(pgcs,df1,df2):
+    #df1 is galbasedf with including r25- get pgc names from this file
+    #df2 is pickle file
+    
     halflights = []
     amps = []
     ns = []
-    datamasks = []
-    nummasked = []
+    mses = []
+
     #print('0')
     for i in np.arange(len(pgcs)):
         print(len(pgcs)-i)
         galmask = df2.gal.isin([pgcs[i]])
         rp = df2.loc[galmask]
+        pgc=pgcs[i]
+        
         if np.isnan(rp.r_arcsec).all()==True:
-            halflights.append('All NaN')
+            halflights.append(np.nan)
             amps.append(np.nan)
             ns.append(np.nan)
-            datamasks.append(np.nan)
-            nummasked.append(np.nan)
+            mses.append(np.nan)
             #print('1')
         elif rp.r_arcsec.min()>200:
-            halflights.append('Bad Decomp')
+            halflights.append(np.nan)
             amps.append(np.nan)
             ns.append(np.nan)
-            datamasks.append(np.nan)
-            nummasked.append(np.nan)
+            mses.append(np.nan)
+
             #print('2')
         else:
             try:
-                r25 = df1.loc[i].R25_DEG*3600
+            
+                rp.r_arcsec/=3600.
+                r25 = df1.loc[i].R25_DEG
                 mask = rp.r_arcsec<2*r25
                 rp = rp[mask]
                 #mask = rp.I>0
                 #rp = rp[mask]
+                ind = np.where(rp.r_arcsec<.5*r25)[0][-1]
                 
-                sersic = models.Sersic1D()
-                outlier_fit = fitting.FittingWithOutlierRemoval(fitting.LevMarLSQFitter(),sigma_clip, niter=3, sigma=2.0)
-                fitted_model,filtered_data = outlier_fit(sersic,rp.r_arcsec,rp.I)
+                sersic = models.Sersic1D(bounds = {'n':(0,14)})
+                outlier_fit = fitting.FittingWithOutlierRemoval(fitting.LevMarLSQFitter(),sigma_clip, niter=3, sigma=2.5)
+                fitted_model,filtered_data = outlier_fit(sersic,rp.r_arcsec,rp.I)#,weights=0.1*rp.I)
+                filtered_data[:ind]=False
+
+                fit = fitting.LevMarLSQFitter()
+                fitted_model = fit(sersic,rp.r_arcsec[~filtered_data],rp.I[~filtered_data])#,weights=(0.1*rp.I[~filtered_data]))
+                mse = np.nanmean((rp.I[~filtered_data] - fitted_model(rp.I[~filtered_data]))**2)
+                print('')
+                print(pgc)
+                #print(np.round(mse,decimals=2))
                 
-                    
-                halflights.append(fitted_model.r_eff.value)
-                amps.append(fitted_model.amplitude.value)
-                ns.append(fitted_model.n.value)
-                datamasks.append(filtered_data)
-                nummasked.append(len(filtered_data==True))
-            except:
-                halflights.append('fit fail')
-                amps.append('fit fail')
-                ns.append('fit fail')
-                datamasks.append('fit fail')
-                nummasked.append('fit fail')
+                """
+                if mse>3.:
+                    ns_mse = []
+                    for n in nrange:
+                        sersic = models.Sersic1D(bounds = {'n':(n,n+1)})
+                        outlier_fit = fitting.FittingWithOutlierRemoval(fitting.LevMarLSQFitter(),sigma_clip, niter=3, sigma=3)
+                        fitted_model,filtered_data_chisq = outlier_fit(sersic,rp.r_arcsec[~filtered_data],rp.I[~filtered_data])#,weights=(0.1*rp.I[~filtered_data]))
+                        m = np.nanmean((rp.I[~filtered_data] - fitted_model(rp.I[~filtered_data]))**2)
+                        if m<mse:
+                            mse=m
+                            ns_mse.append(n)
+                            print(mse,n)
+                        else:
+                            pass
+                    if len(ns_mse)==0:
+                        print(ns_mse)
+                        print('none better')
+                        pass
+                    else:
+                        sersic = models.Sersic1D(bounds = {'n':(ns_mse[-1]-1,ns_mse[-1]+1)})
+                        fitted_model = fit(sersic,rp.r_arcsec[~filtered_data],rp.I[~filtered_data])
+                """
+                re = np.round(fitted_model.r_eff.value*3600*0.9,decimals=3)
+                #re*=0.9
+                #re = np.round(re,decimals=3)
+                n = np.round(fitted_model.n.value,decimals=3)
+                #print(re,saloratio,mm15ratio)
+                #print(n,mmres.n[i],np.round(mmres['T'][i],decimals=2))
 
                 
-    return(pgcs,halflights,amps,ns,datamasks,nummasked)
-        
+                print(re)
+                
+                if re>250.:
+                    halflights.append(np.nan)
+                    amps.append('fit fail')
+                    ns.append('fit fail')
+                    mses.append('fit fail')
+                    
+                elif re<5.:
+                    halflights.append(np.nan)
+                    amps.append('fit fail')
+                    ns.append('fit fail')
+                    mses.append('fit fail')
+                    
+                else:
+                    halflights.append(re)
+                    amps.append(fitted_model.amplitude.value)
+                    ns.append(n)
+                    mses.append(mse)
+            except:
+                halflights.append(np.nan)
+                amps.append('fit fail')
+                ns.append('fit fail')
+                mses.append('fit fail')
+
+
+    redf = pd.DataFrame({'PGC':pgcs,'re':halflights,'amp': amps,'n':ns})
+    #pgcs = np.asarray(pgcs)
+    #halflights = np.asarray(halflights,dtype='float')
+    #amps = np.asarray(amps)
+    #ns = np.asarray(ns)
+    #return(pgcs,halflights,amps,ns,datamasks,nummasked)
+    return(redf)
+    
 def findHalfLightdf(pgcs,df1,df2):
     halflights = []
     #print('0')
